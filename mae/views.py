a5522f9ab4982ce446cde61c17b499d9674a8eb5
Alessandro Hecht <alessandrohc@gmail.com>
2017-05-27 12:40:13 -0300

    Corrigindo o projeto

--- mae/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-#@login_required
-def inicio(request):
-    context = {}
-    return render(request, 'mommy/index.html', context)
-
