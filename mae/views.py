aa1b8c10ef4d55c3aea00fa1344a825a49220a97
Alessandro Hecht <alessandrohc@gmail.com>
2017-05-27 12:25:58 -0300

    Inicio da view no sistema

--- mae/views.py
+++ mae/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+#@login_required
+def inicio(request):
+    context = {}
+    return render(request, 'mommy/index.html', context)
+
