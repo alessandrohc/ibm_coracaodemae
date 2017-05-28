299cca487dd40454eecd210d8960489d9ae42ea1
Alessandro Hecht <alessandrohc@gmail.com>
2017-05-28 04:06:45 -0300

    Habilitando o login simples do django

--- coracaodemae/coracaodemae/urls.py
+++ coracaodemae/coracaodemae/urls.py
@@ -2,15 +2,17 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.contrib import admin
 from mae import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', login_required(views.Inicio.as_view()), name='inicio'),
     url(r'^detalhe/(?P<mae_id>[0-9]+)/$', login_required(views.Detalhe.as_view()), name='detalhe'),
-    url(r'^pagamento/(?P<mae_id>[0-9]+)/$', login_required(views.Pagamento.as_view()), name = 'pagamento'),
+    url(r'^pagamento/(?P<mae_id>[0-9]+)/$', login_required(views.Pagamento.as_view()), name='pagamento'),
     url(r'^confirmacao/(?P<mae_id>[0-9]+)/$', login_required(views.Confirmacao.as_view()), name='confirmacao'),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'mae/login.html'}, name="login"),
+    url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}, name="logout"),
 
-    # url(r'^accounts/login/$', views.login),
     # url(r'^cadastro_mae/$', views.cadastro_mae),
     # url(r'^lista/$', views.lista),
     url(r'^avaliacao/(?P<mae_id>[0-9]+)/$', views.avaliacao),
