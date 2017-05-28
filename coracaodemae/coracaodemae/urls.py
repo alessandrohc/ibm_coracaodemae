8d453f3fd2c34439b666a047a2ba2c984d83512d
Alessandro Hecht <alessandrohc@gmail.com>
2017-05-28 04:08:58 -0300

    Corrigindo o merge

--- coracaodemae/coracaodemae/urls.py
+++ coracaodemae/coracaodemae/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from mae import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
