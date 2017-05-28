14deefd01f49b1189e9134c1d2d08c818628deca
Alessandro Hecht <alessandrohc@gmail.com>
2017-05-28 11:58:30 -0300

    Bug do templatetag

--- coracaodemae/mae/templatetags/bootstrapget.py
+++ coracaodemae/mae/templatetags/bootstrapget.py
@@ -1,7 +1,11 @@
 from django import template
+
 register = template.Library()
 
 
 @register.filter
 def hash(h, key):
-    return [h[key]]
+    try:
+        return [h[key]]
+    except:
+        return []
