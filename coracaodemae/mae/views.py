84b77a15a93dc0474f0ce5ff875edb37915978ed
Alessandro Hecht <alessandrohc@gmail.com>
2017-05-27 21:58:11 -0300

    Deixando preparado as funcoes para retornar corretamente o index e detalhe

--- coracaodemae/mae/views.py
+++ coracaodemae/mae/views.py
@@ -1,31 +1,50 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, DeleteView, ListView, View, TemplateView, DetailView
+from mae.models import *
+from django.db.models import Avg, Count, Sum
 
 
-#@login_required
-def inicio(request):
-    context = {}
-    return render(request, 'mae/index.html', context)
+class Inicio(ListView):
+    """
+    Url para Exibir os detalhes do Post
+    """
+    model = Mae
+
+    template_name = 'mae/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Inicio, self).get_context_data(**kwargs)
+
+        context['user'] = self.request.user
+        context['maes'] = Mae.objects.all()
+
+        return context
 
 
-def login(request):
-    context = {}
-    return render(request, 'mae/login.html', context)
+class Detalhe(ListView):
 
+    model = Mae
 
-def cadastro_mae(request):
-    context = {}
-    return render(request, 'mae/cadastro_mae.html', context)
+    template_name = 'mae/detalhe.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(Detalhe, self).get_context_data(**kwargs)
+        mae_id = self.kwargs.pop('mae_id', None)
+        mae_obj = Mae.objects.get(pk=mae_id)
+        context['user'] = self.request.user
+        context['mae'] = mae_obj
+        context['qtd_filho'] = mae_obj.filho_set.count()
 
-def lista(request):
-    context = {}
-    return render(request, 'mae/lista.html', context)
+        context['comentarios'] = ComentarioMae.objects.filter(mae_destino=mae_obj)
 
+        aggregates = AvaliacaoMae.objects.filter(mae_destino=mae_obj).aggregate(
+            total=Sum('avaliacao'), average=Avg('avaliacao'), count=Count('avaliacao'))
+        # count = aggregates.get('count') or 0
+        # total = aggregates.get('total') or 0
+        average = aggregates.get('average') or 0.0
+        context['avaliacao'] = str(average).replace(",", ".")
 
-def detalhe(request, param):
-    context = {}
-    return render(request, 'mae/detalhe.html', context)
+        return context
 
 
 def pagamento(request, param):
@@ -41,3 +60,13 @@ def confirmacao(request, param):
 def avaliacao(request, param):
     context = {}
     return render(request, 'mae/avaliacao.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'mae/login.html', context)
+
+
+def cadastro_mae(request):
+    context = {}
+    return render(request, 'mae/cadastro_mae.html', context)
